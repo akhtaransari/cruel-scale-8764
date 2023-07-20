f4b1774a9b407bffe74f65c3f82f80870b500618
Akhtar Ansari <akhtaransari.info@gmail.com>
2023-07-20 23:30:27 +0530

    Day 2 work done with login and resigter page

---
+++ Front-End/scripts/login.js
@@ -0,0 +1,32 @@
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+const loginDiv = document.getElementById("login");
+const loginContainer = document.querySelector(".container");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
+
+loginDiv.addEventListener("click",()=>{loginContainer.style.display = "flex"});
+
